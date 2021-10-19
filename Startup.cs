4091d394345c448a40495155c26b902adbbfc868
felipeholanda2077 <felipe_holandafreitas@hotmail.com>
2021-10-19 18:17:14 -0300

    ATV-2

--- Startup.cs
+++ Startup.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -20,9 +22,18 @@ namespace ATV_2_UC4
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        [Obsolete]
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+        services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => false;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMemoryCache();
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,7 +46,15 @@ namespace ATV_2_UC4
             else
             {
                 app.UseExceptionHandler("/Home/Error");
+                //Https
+                app.UseHsts();
             }
+            //Https
+            app.UseHttpsRedirection();
+            app.UseCookiePolicy();
+            app.UseSession();
+            //dotnet dev-certs https --trust
+            
             app.UseStaticFiles();
 
             app.UseRouting();
